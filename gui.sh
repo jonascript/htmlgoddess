efc2ec29868e70bff58dd4986e8e03f13dd90cbe
Jonathan Crockett <joncrockett@gmail.com>
2020-06-04 16:44:16 -0400

    saving work

--- gui.sh
+++ gui.sh
@@ -9,12 +9,14 @@ Suitcase basic --name="HTML Goddess" --window-title="HTML Goddess" \
   --control-type="button" \
   --control-title="Print" \
   --control-action="/usr/local/bin/npm run print" \
+  --control-action-destination="output" \
   --control-type="button" \
   --control-title="Save" \
   --control-action="/usr/local/bin/npm run save" \
+  --control-action-destination="output" \
   --control-type="button" \
   --control-title="Publish" \
-  --control-action="/usr/local/bin/npm run save" \
+  --control-action="/usr/local/bin/npm run publish" \
   --control-action-destination="output" \
   --control-title="Console" \
   --control-type="text" \
