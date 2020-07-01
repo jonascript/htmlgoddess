553a3e4c2ae8025b66707b02f85906164424e1d4
Jonathan Crockett <joncrockett@gmail.com>
2020-07-01 14:52:06 -0400

    fix: 🐛 removing spellchecker to fix install issue

--- packages/@htmlgoddess/templates/index.js
+++ packages/@htmlgoddess/templates/index.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 module.exports.getTemplatePath = function (templateName) {
   if (!templateName) {
-    console.log('Yohoho', templateName);
     return false;
   }
 
